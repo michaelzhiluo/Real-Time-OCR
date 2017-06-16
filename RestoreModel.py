455fd26ad7832f6e24baa824affb9bea4ebd3cdf
Michael Luo - 羅志宇 <michael.luo@berkeley.edu>
2017-06-16 15:38:23 +0800

    Restored RestoreModel.py

--- RestoreModel.py
+++ RestoreModel.py
@@ -16,32 +16,11 @@ keep_prob = graph.get_tensor_by_name("keep_prob:0")
 
 predict = graph.get_tensor_by_name("predicted_number:0")
 
-originalimg = cv2.imread("Three.jpg")
-resize = cv2.resize(originalimg, dsize =(28, 28))
-gray = cv2.cvtColor(resize, cv2.COLOR_BGR2GRAY)
-print(gray)
-average = cv2.mean(gray)
-if(gray[0][0] < average[0]):
-    gray = 255 - gray
-_,thres = cv2.threshold(gray, 255-average[0], 0, 2)
-print(average[0])
-nump = np.asarray(thres, np.float32)
-nump = 255 - nump
-nump = (nump -np.min(nump))/(np.max(nump) - np.min(nump))
-plt.imshow(nump, cmap = 'gray')
-plt.show()
-plt.imshow(mnist.test.images[0].reshape(28, 28), cmap = 'gray')
-plt.show()
+t_data = mnist.test.images[:128]
+t_label = mnist.test.labels[:128]
 
-prediction = sess.run(predict, feed_dict ={training_data: [nump.flatten()] ,training_labels: [[0,0,0,0,0,0,0,0,0,1]], keep_prob: 1})
+prediction = sess.run(predict, feed_dict ={training_data: t_data ,training_labels: t_label, keep_prob: 1})
 print(prediction)
-
-'''
-video = cv2.VideoCapture(0)
-while(True):
-    ret, frame = video.read()   
-    
-    cv2.imshow('test', frame)
-    cv2.waitKey(5)
-
-'''
+temp = np.argmax(t_label, 1) 
+print(temp)
+print(np.equal(prediction, temp))
