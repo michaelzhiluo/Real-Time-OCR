283016abcc5719295f2c48b778dcdf2d9523778b
Michael Luo - 羅志宇 <michael.luo@berkeley.edu>
2017-06-23 10:24:55 +0800

    Can convert to MNIST Good progress

--- ConvertToMNIST.py
+++ ConvertToMNIST.py
@@ -1,15 +1,27 @@
-import numpy as np
-import tensorflow as tf
 import cv2
-import matplotlib.pyplot as plt
+import numpy as np
 from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
 
-def clusterImage(image, numclusters):
-	convert = image.reshape((image.shape[0]*image.shape[1],3))
-	convert = np.float32(convert)
-	criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
-	ret,label,center=cv2.kmeans(convert,numclusters,None,criteria,10,cv2.KMEANS_RANDOM_CENTERS)
-	center = np.uint8(center)
-	ans = center[label.flatten()]
-	res2 = ans.reshape((image.shape))
-	return res2
+def convertImageToMNIST(img):
+	resize = cv2.resize(img, dsize =(28, 28))
+	gray = cv2.cvtColor(resize, cv2.COLOR_BGR2GRAY)
+	average = cv2.mean(gray)
+	if(gray[0][0] < average[0]):
+		gray = 255 - gray
+	_,thres = cv2.threshold(gray, 255-average[0], 0, 2)
+	num = np.asarray(thres, np.float32)
+	num = 255 -num
+	num = (num -np.min(num))/(np.max(num) - np.min(num))
+	return num
+
+def clusterImage(img, numclusters):
+    convert = img.reshape((img.shape[0]*img.shape[1],3))
+    convert = np.float32(convert)
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+    ret,label,center=cv2.kmeans(convert,numclusters,None,criteria,10,cv2.KMEANS_RANDOM_CENTERS)
+    center = np.uint8(center)
+    ans = center[label.flatten()]
+    res2 = ans.reshape((img.shape))
+    return res2
+
--- RestoreModel.py
+++ RestoreModel.py
@@ -2,9 +2,8 @@ import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
 import cv2
-from tensorflow.examples.tutorials.mnist import input_data
+import ConvertToMNIST
 
-mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
 sess=tf.Session()    
 saver = tf.train.import_meta_graph('C:\\MyModel\\CNNModel.meta')
 saver.restore(sess,tf.train.latest_checkpoint('C:\\MyModel'))
@@ -16,11 +15,24 @@ keep_prob = graph.get_tensor_by_name("keep_prob:0")
 
 predict = graph.get_tensor_by_name("predicted_number:0")
 
-t_data = mnist.test.images[:128]
-t_label = mnist.test.labels[:128]
+img = cv2.imread("Seven.jpg")
+matrix = ConvertToMNIST.clusterImage(img, 3);
+temp = ConvertToMNIST.convertImageToMNIST(matrix)
 
-prediction = sess.run(predict, feed_dict ={training_data: t_data ,training_labels: t_label, keep_prob: 1})
-print(prediction)
-temp = np.argmax(t_label, 1) 
-print(temp)
-print(np.equal(prediction, temp))
+plt.imshow(temp, cmap = 'gray')
+plt.show()
+'''
+video = cv2.VideoCapture(0)
+while(True):
+    ret, frame = video.read()   
+    
+    cv2.imshow('test', frame)
+    cv2.waitKey(5)
+
+'''
+#prediction = sess.run(predict, feed_dict ={training_data: t_data ,training_labels: t_label, keep_prob: 1})
+
+#print(prediction)
+#temp = np.argmax(t_label, 1) 
+#print(temp)
+#print(np.equal(prediction, temp))
