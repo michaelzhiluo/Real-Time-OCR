25a3b00c47525137b041cd871900db4f1df7e205
Michael Luo - 羅志宇 <michael.luo@berkeley.edu>
2017-06-30 16:52:24 +0800

    Generated Computer MNIST Dataset, Images and Labels in Separate Files

--- GenerateComputerMNISTFile.py
+++ GenerateComputerMNISTFile.py
@@ -4,29 +4,39 @@ import ConvertToMNIST
 from PIL import Image, ImageFilter
 import random
 
+def fillImagesLabels(numclasses, imagesperclass, filename):
+	images = []
+	labels = []
+	for i in range(0, numclasses):
+		for j in range(0, imagesperclass):		
+			image = cv2.imread(filename + "\\"  + str(i) + "\\" + str(j) + ".jpg")
+			clustered = ConvertToMNIST.clusterImage(image)
+			final = ConvertToMNIST.imageprepare(Image.fromarray(clustered))
+			images += [final]
+			temp = [0,0,0,0,0,0,0,0,0,0]
+			temp[i] = 1
+			labels+=[temp]
+	return images, labels
+
+def mutualShuffle(images, labels):
+	shuffle_indexes = []
+	for i in range(0, len(images)):
+		shuffle_indexes.append(i)
+	random.shuffle(shuffle_indexes)
+	images_shuf = []
+	labels_shuf = []
+	for i in shuffle_indexes:
+		images_shuf +=[images[i]]
+		labels_shuf +=[labels[i]]
+	return np.array(images_shuf), np.array(labels_shuf)
+
+
+def save(array, name):
+	np.save(name + ".npy", array)
+
 images = []
 labels = []
-for i in range(0, 10):
-	for j in range(0, 500):		
-		temp = cv2.imread("ComputerMNIST\\"  + str(i) + "\\" + str(j) + ".jpg")
-		temp = ConvertToMNIST.clusterImage(temp)
-		temp = ConvertToMNIST.imageprepare(Image.fromarray(temp))
-		images += [temp]
-		temp = [0,0,0,0,0,0,0,0,0,0]
-		temp[i] = 1
-		labels+=[temp]
- 	
-shuffle_indexes = []
-print(len(images))
-for i in range(0, len(images)):
-	shuffle_indexes.append(i)
-random.shuffle(shuffle_indexes)
-images_shuf = []
-labels_shuf = []
-for i in shuffle_indexes:
-	images_shuf +=[images[i]]
-	labels_shuf +=[labels[i]]
-images_shuf = np.array(images_shuf)
-labels_shuf = np.array(labels_shuf)
-np.save("ComputerMNISTImages.npy", images_shuf)
-np.save("ComputerMNISTLabels.npy", labels_shuf)
+images, labels = fillImagesLabels(10, 500, "ComputerMNIST")
+images, labels = mutualShuffle(images, labels)
+save(images, "ComputerMNISTImages")
+save(labels, "ComputerMNISTLabels")
