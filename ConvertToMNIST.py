abf690190f2ac4530abd9a6416db584bd0de65a1
Michael Luo - 羅志宇 <michael.luo@berkeley.edu>
2017-07-03 16:02:23 +0800

    Updated API

--- ConvertToMNIST.py
+++ ConvertToMNIST.py
@@ -3,6 +3,23 @@ import numpy as np
 from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
 from PIL import Image, ImageFilter
+
+def clusterImage(img):
+    convert = img.reshape((img.shape[0]*img.shape[1],3))
+    convert = np.float32(convert)
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+    ret,label,center=cv2.kmeans(convert,2,None,criteria,10,cv2.KMEANS_RANDOM_CENTERS)
+    center = np.uint8(center)
+    if(difference(center[0], img[0][0]) > difference(center[1], img[0][0])):
+    	center[1] = [255.0, 255.0, 255.0]
+    	center[0] = [0, 0, 0]
+    else:
+    	center[0] = [255.0, 255.0, 255.0]
+    	center[1] = [0, 0, 0]
+    ans = center[label.flatten()]
+    res2 = ans.reshape((img.shape))
+    return res2
+
 def imageprepare(im):
   #im = Image.open(argv).convert('L')
   width = float(im.size[0])
@@ -31,34 +48,7 @@ def imageprepare(im):
   tva = [ (255-x)*1.0/255.0 for x in tv] 
   return tva
 
-def convertImageToMNIST(img):
-	resize = cv2.resize(img, dsize =(28, 28))
-	gray = cv2.cvtColor(resize, cv2.COLOR_BGR2GRAY)
-	average = cv2.mean(gray)
-	if(gray[0][0] < average[0]):
-		gray = 255 - gray
-	_,thres = cv2.threshold(gray, 255-average[0], 0, 2)
-	num = np.asarray(thres, np.float32)
-	num = 255 -num
-	num = (num -np.min(num))/(np.max(num) - np.min(num))
-	return num
-
-def clusterImage(img):
-    convert = img.reshape((img.shape[0]*img.shape[1],3))
-    convert = np.float32(convert)
-    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
-    ret,label,center=cv2.kmeans(convert,2,None,criteria,10,cv2.KMEANS_RANDOM_CENTERS)
-    center = np.uint8(center)
-    if(difference(center[0], img[0][0]) > difference(center[1], img[0][0])):
-    	center[1] = [255.0, 255.0, 255.0]
-    	center[0] = [0, 0, 0]
-    else:
-    	center[0] = [255.0, 255.0, 255.0]
-    	center[1] = [0, 0, 0]
-    ans = center[label.flatten()]
-    res2 = ans.reshape((img.shape))
-    return res2
-
+# Not part of API
 def difference(cluster, pixel):
 	c = np.int16(cluster)
 	p = np.int16(pixel)
