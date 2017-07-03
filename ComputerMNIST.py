43637bbcc028d33c5125dcb69f793a6a9004906f
Michael Luo - 羅志宇 <michael.luo@berkeley.edu>
2017-07-03 08:57:15 +0800

    Made ComputerMNISTClass

--- ComputerMNIST.py
+++ ComputerMNIST.py
@@ -1,5 +1,21 @@
+import numpy as np
 
 
 class ComputerMNIST:
-	def ComputerMNIST():
-	def nextBatch(i):
+
+	def ComputerMNIST(self):
+		self.images = np.load("ComputerMNISTImages.npy")
+		self.labels = np.load("ComputerMNISTLabels.npy")
+		self.num_examples = len(images)
+
+	def next_batch(batch_size):
+
+
+	@property
+	def images(self):
+		return self.images
+
+	@property
+	def labels(self):
+		return self.labels
+
