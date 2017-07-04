aa78d89ac839c5bf24b3cbf57364bfb677dce6f8
Michael Luo - 羅志宇 <michael.luo@berkeley.edu>
2017-07-04 11:15:37 +0800

    Done

--- RestoreModel.py
+++ RestoreModel.py
@@ -10,7 +10,7 @@ from PIL import Image, ImageFilter
 
 sess=tf.Session()    
 saver = tf.train.import_meta_graph('MyModel\\CNNModel.meta')
-saver.restore(sess,tf.train.latest_checkpoint('\\MyModel'))
+saver.restore(sess,tf.train.latest_checkpoint('MyModel'))
 
 graph = tf.get_default_graph()
 training_data = graph.get_tensor_by_name("training_data:0")
@@ -19,7 +19,7 @@ keep_prob = graph.get_tensor_by_name("keep_prob:0")
 
 predict = graph.get_tensor_by_name("predicted_number:0")
 
-counter =0
+counter =11000
 roi = []
 # 0 -> x1, 1 -> x2, 2 -> y1, 3-> y2
 roi += [[320, 345, 115, 175]]
@@ -42,12 +42,14 @@ while(counter <=20000):
 	prediction = [] 
 	for i in range(0, len(roi)):
 		temp = frame[roi[i][2]: roi[i][3], roi[i][0]: roi[i][1]]
-	for i in range(0, len(roimat)):
-		cv2.rectangle(frame, (roi[i][0], roi[i][2]), (roi[i][1], roi[i][3]), (255, 255, 255))
-		temp = ConvertToMNIST.clusterImage(roimat[i])
+		temp = ConvertToMNIST.clusterImage(temp)
 		temp = ConvertToMNIST.imageprepare(Image.fromarray(temp))
-		prediction = sess.run(predict, feed_dict ={training_data: [temp] ,training_labels: [[1,0,0,0,0,0,0,0,0,0]], keep_prob: 1})
-		cv2.putText(frame, str(prediction[0]), (roi[i][0] - 10, roi[i][2] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.65, (0, 255, 0), 2)
+		pred = sess.run(predict, feed_dict ={training_data: [temp] ,training_labels: [[1,0,0,0,0,0,0,0,0,0]], keep_prob: 1})
+		prediction += [pred[0]]
+
+	for i in range(0, len(roi)):
+		cv2.rectangle(frame, (roi[i][0], roi[i][2]), (roi[i][1], roi[i][3]), (255, 255, 255))
+		cv2.putText(frame, str(prediction[i]), (roi[i][0] - 10, roi[i][2] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.65, (0, 255, 0), 2)
 	cv2.imshow('frame',frame)
 	if cv2.waitKey(1) & 0xFF == ord('q'):
 		break
